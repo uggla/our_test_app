960d1199a5477b397a65a63e8433749e05ae3b14
Uggla <uggla@free.fr>
2017-10-16 11:19:11 +0200

    Debugging #3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Deploy script
 # staging will go on Digital Ocean
 # prod will go on openstack on premice
-
+echo "$1 phase"
 source script/openrc.sh
 export OS_PASSWORD="$OS_PASSWORD"
 openstack server list
