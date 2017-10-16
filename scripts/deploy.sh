8cd22a1954fadb066165af3719b1ed14c3a3b8b9
Uggla <uggla@free.fr>
2017-10-16 13:37:44 +0200

    Try to deploy a VM on ovh openstack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,10 @@
 # staging will go on Digital Ocean
 # prod will go on openstack on premice
 echo "$1 phase"
-echo $(pwd)
 source scripts/openrc.sh
 export OS_PASSWORD="$OS_PASSWORD"
 openstack server list
+
+echo "Commit: $TRAVIS_COMMIT"
+
+openstack server create --image 'Debian 8 - Docker' --flavor "vps-ssd-1" --key-name uggla d1_$TRAVIS_COMMIT
