14a5b7f11550dff13166718a0c52f8249f5e6802
Uggla <uggla@free.fr>
2017-10-16 11:14:56 +0200

    Debugging #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,6 @@
 # staging will go on Digital Ocean
 # prod will go on openstack on premice
 
-echo "Test secure key"
-echo "SOMEVAR=$SOMEVAR"
+source script/openrc.sh
+export OS_PASSWORD="$OS_PASSWORD"
+openstack server list
